694b059c5faba39cdae17b52a6ffb7bd3b391322
Adri <adrianvillalbasanchez@gmail.com>
2019-06-16 18:35:58 +0200

    Updates

---
+++ precache-manifest.a5d5a386a299b519716b8b1a1cff31b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "017bcde69a4881e8578df0f0cbd4d3b2",
+    "url": "/clover-front/index.html"
+  },
+  {
+    "revision": "0c2de970e84d940d8f0f",
+    "url": "/clover-front/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "9937a8c1ecb691ee861c",
+    "url": "/clover-front/static/css/main.1d187945.chunk.css"
+  },
+  {
+    "revision": "0c2de970e84d940d8f0f",
+    "url": "/clover-front/static/js/2.a513f9f7.chunk.js"
+  },
+  {
+    "revision": "9937a8c1ecb691ee861c",
+    "url": "/clover-front/static/js/main.9cd09814.chunk.js"
+  },
+  {
+    "revision": "ae1f0338ab8066b41c49",
+    "url": "/clover-front/static/js/runtime~main.55f06c06.js"
+  }
+]);
